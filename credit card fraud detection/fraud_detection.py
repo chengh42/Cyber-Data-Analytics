fc0ea7c3a87d788d6719665ae0ba6af4c111b2fe
Hsin Cheng <44002425+imchengh@users.noreply.github.com>
2019-05-11 08:54:34 +0200

    Overwrite hand-in version

--- credit card fraud detection/fraud_detection.py
+++ credit card fraud detection/fraud_detection.py
@@ -116,12 +116,6 @@ df.simple_journal = label_binarize(['Chargeback'], classes = df.simple_journal).
 df.reset_index(inplace = True)  # Reseting index due to drops
 df.drop(columns = ['txid'], inplace = True) # drop txid
 
-# arrange df per card and date of transaction for amount aggregation
-df.sort_values(by = ['card_id', 'creationdate'], inplace = True)
-
-## get the aggreagted amount of transaction per card
-df['x_amount'] = df.groupby(by = 'card_id').expanding(1).amount.mean().values
-
 # check dataset
 df.info()
 #df.sample(5)
@@ -137,12 +131,119 @@ feature_names = df.drop(columns = ['simple_journal', 'bookingdate']).columns.val
 
 # %% [markdown]
 # 
+# ## 1. Aggregate features (bonus task) ##
+# 
+# Create a new dataframe for only aggregated features of **the past transactions frequency/amount per credit card (card_id)** at
+# 
+# + the same merchant
+# + the same commerce type (online, etc.)
+# + the same country where transaction took place
+# 
+
+# %%
+# Bonus assigment - adding context variables
+'''
+Note: This subpart takes ~20 minutes to run
+The result is saved in .csv file './data/aggregated_data.csv'
+'''
+# If necessary, load exported data
+# df = pd.read_csv('./data/aggregated_data.csv')
+
+# Creating the frequency column that counts how many times the same card appears
+df['creditcard_frequency'] = df.groupby('card_id')['card_id'].transform('count')
+
+# Defining default/dummy values for new variables
+df['amountSpent'] = 0 # Variables related to the expenditure
+df['SpentWithSameMerchant'] = 0 # Variables related to the merchant
+df['SpentWithECommerce'] = 0 # E-commerce
+df['SpentInTheCountry'] = 0 # Country
+
+#AmountSpent in the past
+def amountSpent(creationdate,card_id):
+    return float(df[(df["creationdate"] < creationdate) & (df["card_id"] == card_id)][['amount']].sum())
+
+#Average spent per operation
+def AVGamountSpent(creationdate,card_id):
+    counting = float(df[(df["creationdate"] < creationdate) & (df["card_id"] == card_id)][['amount']].count())
+    if counting > 0:
+        return float(df[(df["creationdate"] < creationdate) & (df["card_id"] == card_id)][['amount']].sum())/counting
+    else:
+        return 0
+  
+#Amount spent in the past with the same merchant
+def SpentWithSameMerchant(creationdate,card_id,accountcode):
+    return float(df[(df["creationdate"] < creationdate) & (df["card_id"] == card_id) & (df["accountcode"] == accountcode)][['amount']].sum())
+
+#Average spent in the past per operation with the same merchant
+def AVGSpentWithSameMerchant(creationdate,card_id,accountcode):
+    counting = float(df[(df["creationdate"] < creationdate) & (df["card_id"] == card_id) & (df["accountcode"] == accountcode)][['amount']].count())
+    if counting>0:
+        return float(df[(df["creationdate"] < creationdate) & (df["card_id"] == card_id) & (df["accountcode"] == accountcode)][['amount']].sum())/counting
+    else:
+        return 0
+
+#Amount spent by type of commerce in the past
+def SpentWithECommerce(creationdate,card_id,shopperinteraction):
+    return float(df[(df["creationdate"] < creationdate) & (df["card_id"] == card_id) & (df["shopperinteraction"] == shopperinteraction)][['amount']].sum())
+
+#Average spent in the past per operation by type of commerce 
+def AVGSpentWithECommerce(creationdate,card_id,shopperinteraction):
+    counting=float(df[(df["creationdate"] < creationdate) & (df["card_id"] == card_id) & (df["shopperinteraction"] == shopperinteraction)][['amount']].count())
+    if counting>0:
+        return float(df[(df["creationdate"] < creationdate) & (df["card_id"] == card_id) & (df["shopperinteraction"] == shopperinteraction)][['amount']].sum())/counting
+    else:
+        return 0
+
+#Amount spent in the country previously
+def SpentInTheCountry(creationdate,card_id,shoppercountrycode):
+    return float(df[(df["creationdate"] < creationdate) & (df["card_id"] == card_id) & (df["shoppercountrycode"] == shoppercountrycode)][['amount']].sum())
+
+#Average spent in the country per operation in the past
+def AVGSpentInTheCountry(creationdate,card_id,shoppercountrycode):
+    counting=float(df[(df["creationdate"] < creationdate) & (df["card_id"] == card_id) & (df["shoppercountrycode"] == shoppercountrycode)][['amount']].count())
+    if counting>0:
+        return float(df[(df["creationdate"] < creationdate) & (df["card_id"] == card_id) & (df["shoppercountrycode"] == shoppercountrycode)][['amount']].sum())/counting
+    else:
+        return 0
+
+df['amountSpent'] = df.apply(lambda df: amountSpent(df["creationdate"], df["card_id"]) if df["creditcard_frequency"] >1 else 0, axis=1)
+df['AVGamountSpent'] = df.apply(lambda df: AVGamountSpent(df["creationdate"], df["card_id"]) if df["creditcard_frequency"] >1 else 0, axis=1)
+df['SpentWithSameMerchant'] = df.apply(lambda df: SpentWithSameMerchant(df["creationdate"], df["card_id"], df['accountcode'])if df["amountSpent"] >0 else 0, axis=1)
+df['AVGSpentWithSameMerchant'] = df.apply(lambda df: AVGSpentWithSameMerchant(df["creationdate"], df["card_id"], df['accountcode'])if df["amountSpent"] >0 else 0, axis=1)
+df['SpentWithECommerce'] = df.apply(lambda df: SpentWithECommerce(df["creationdate"], df["card_id"], df['shopperinteraction'])if df["amountSpent"] >0 else 0, axis=1)
+df['AVGSpentWithECommerce'] = df.apply(lambda df: AVGSpentWithECommerce(df["creationdate"], df["card_id"], df['shopperinteraction'])if df["amountSpent"] >0 else 0, axis=1)
+df['SpentInTheCountry'] = df.apply(lambda df: SpentInTheCountry(df["creationdate"], df["card_id"], df['shoppercountrycode'])if df["amountSpent"] >0 else 0, axis=1)
+df['AVGSpentInTheCountry'] = df.apply(lambda df: AVGSpentInTheCountry(df["creationdate"], df["card_id"], df['shoppercountrycode'])if df["amountSpent"] >0 else 0, axis=1)
+  
+#Ratio variables - dividing amount of the transaction to previous transactions
+df['RatioAmountToAmountSpent']=df.apply(lambda df: df["amountSpent"]/df["amount"] if df["amountSpent"]>0  else 0, axis=1)
+df['RatioAmountToAmountSpentMerchant']=df.apply(lambda df: df["SpentWithSameMerchant"]/df["amount"] if df["SpentWithSameMerchant"]>0  else 0, axis=1)
+df['RatioAmountToAmountSpentECommerce']=df.apply(lambda df: df["SpentWithECommerce"]/df["amount"] if df["SpentWithECommerce"]>0  else 0, axis=1)
+df['RatioAmountToAmountSpentCountry']=df.apply(lambda df: df["SpentInTheCountry"]/df["amount"] if df["SpentInTheCountry"]>0  else 0, axis=1)
+
+df.info() #check dataframe
+
+df.to_csv(r'./data/aggregated_data.csv') #export to csv file
+
+## Features & Labels
+y_agg = df.simple_journal.values  # labels
+x_agg = df.drop(columns = ['simple_journal', 'bookingdate', 'creditcard_frequency']).values  # features
+feature_names_agg = df.drop(columns = ['simple_journal', 'bookingdate', 'creditcard_frequency']).columns.values
+
+
+# %% [markdown]
+# 
 # ## 2. Visualisation task ##
 # Make a visualization showing an interesting relationship in the data when comparing the fraudulent from the benign credit card transactions. You may use any visualization method such as a Heat map, a Scatter plot, a Bar chart, a set of Box plots, etc. as long as they show all data points in one figure. What feature(s) and relation to show is entirely up to you. Describe the plot briefly. 
 #
 # %%
 
-sns.set(style = 'whitegrid') #set graph style
+'''
+## Task 1
+## Visualisation
+'''
+
+sns.set(style = 'whitegrid')
 
 # get a subset of df which contains 1/4 fraud and 3/4 benign
 df_a = df.loc[df.simple_journal == 1]
@@ -164,7 +265,8 @@ plt.savefig('df_a.png')
 
 # %%
 
-## subset data for only few columns which seem to have stronger correlations
+## subset data for only few columns 
+##   which seem 'correlated'
 
 ## Features & Labels
 y_sub = df.simple_journal.values  # labels
@@ -178,7 +280,12 @@ feature_names_sub = ['issuercountrycode', 'amount', 'cvcresponsecode', 'accountc
 # 
 # %%
 
-sns.set(style = 'whitegrid') #set graph style
+'''
+##Task 2
+##Imbalanced data: standard vs SMOTE
+'''
+
+sns.set(style = 'whitegrid')
 
 # settings
 cv = StratifiedKFold(n_splits = 5, random_state = seed)
@@ -205,15 +312,14 @@ plot_roc_curve(classifiers, data, cv, title = "ROC curve: SMOTE'd vs non-SMOTE'd
 #
 # Explain the applied data pre-processing steps, learning algorithms, and post-processing steps or ensemble methods. Compare the performance of the two algorithms, focusing on performance criteria that are relevant in practice, use 10-fold cross-validation. Write clear code/scripts for this task, for peer-review your fellow students will be asked to run and understand your code! 
 # 
-# %% [markdown]
-# 
-# 
-# ## 4.1 Blackbox algorithm
-# 
-
 # %%
 
-sns.set(style = 'whitegrid') #set graph style
+'''
+##Task 3-1
+##Classification : Blackbox
+'''
+
+sns.set(style = 'whitegrid')
 
 # settings
 rs = ShuffleSplit(n_splits = 10, test_size = 0.1, random_state = seed)
@@ -238,14 +344,28 @@ plot_roc_curve(classifiers, data, rs, title = 'ROC curve: blackbox classifiers',
 plot_roc_curve(classifiers, data_sub, rs, title = 'ROC curve: blackbox classifiers - subseted features',
                filename = 'ROC curve Blackbox sub.png')
 
-# %% [markdown]
-# 
-# ## 4.2 Whitebox algorithm
-# 
+# %%
+
+## Run KNN classifiers with different nr of neighbors
+
+classifiers = [
+    ['3NN', KNeighborsClassifier(n_neighbors = 3, weights = 'distance', n_jobs = -1)],
+    ['5NN', KNeighborsClassifier(n_neighbors = 5, weights = 'distance', n_jobs = -1)],
+    ['10NN', KNeighborsClassifier(n_neighbors = 10, weights = 'distance', n_jobs = -1)],
+]
+
+# plot roc curve for df w/ subseted features
+plot_roc_curve(classifiers, data_sub, rs, title = 'ROC curve: KNN classifiers - subseted features',
+               filename = 'ROC curve Blackbox KNN sub.png')
 
 # %%
 
-sns.set(style = 'whitegrid') #set graph style
+'''
+##Task 3-2
+##Classification : Whitebox
+'''
+
+sns.set(style = 'whitegrid')
 
 # settings
 rs = ShuffleSplit(n_splits = 10, test_size = 0.1, random_state = seed)
@@ -256,15 +376,15 @@ classifiers = [
     ['SGDClassifier', SGDClassifier(random_state = seed, n_jobs = -1)]
 ]
 data = [x, y]
-data_sub = [x_sub, y_sub]
+data_agg = [x_agg, y_agg]
 
 # plot roc curve for whitebox algoritms
 plot_roc_curve(classifiers, data, rs, title = 'ROC curve: whitebox classifiers',
                filename = 'ROC curve Whitebox.png')
 
-# plot roc curve for whitebox algoritms w/ subseted features
-plot_roc_curve(classifiers, data_sub, rs, title = 'ROC curve: whitebox classifiers w/ subseted features',
-               filename = 'ROC curve Whitebox sub.png')
+# plot roc curve for df w/ aggregated features
+plot_roc_curve(classifiers, data_agg, rs, title = 'ROC curve: whitekbox classifiers w/ aggregated features',
+               filename = 'ROC curve Whitekbox agg.png')
 
 # %%
 
