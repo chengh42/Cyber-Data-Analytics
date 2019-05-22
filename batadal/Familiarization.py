e08b6898fff17dc0c386bfa7a59458b2197a7487
Hsin Cheng <44002425+imchengh@users.noreply.github.com>
2019-05-22 23:07:57 +0200

    Update Familiarization.py

--- batadal/Familiarization.py
+++ batadal/Familiarization.py
@@ -13,14 +13,11 @@ import pandas as pd
 import numpy as np
 import time, datetime
 import matplotlib.pyplot as plt
-
-# %% function for tranforming string date to timestamp and vice versa
-
-def string_to_timestamp(s):
-    return datetime.datetime.strptime(s, "%d/%m/%y %H").timestamp()
-
-def timestamp_to_date(t):
-    return datetime.datetime.fromtimestamp(t).strftime('%m-%d %H') #to covert timestamp to datetime
+import seaborn as sns
+from pandas.plotting import autocorrelation_plot
+from statsmodels.tsa.arima_model import ARIMA
+from sklearn.metrics import mean_squared_error
+from multiprocessing import Pool
 
 # %% [markdown]
 
@@ -40,7 +37,7 @@ dftrain1.sample(n = 10)
 dftrain1.info()
 
 # Modify string date to timestamp
-dftrain1.DATETIME = dftrain1.DATETIME.apply(lambda s: string_to_timestamp(s))
+dftrain1.DATETIME = dftrain1.DATETIME.apply(lambda s: pd.to_datetime(s, format = '%d/%m/%y %H'))
 
 # %% [markdown]
 #
@@ -111,6 +108,190 @@ for hr in time_scale:
     plt.savefig('.\image\df1\Familiarization_Setting_%ihr.png' % hr, 
                 bbox_inches = 'tight', facecolor = 'w')
 
+# %% [markdown]
+# 
+# ### Correlated behaviour
+# 
+
+# %% correlation matrix
+
+corr = np.corrcoef(dftrain1.drop(columns = ['DATETIME', 'ATT_FLAG']))
+
+np.correlate(dftrain1.L_T3.values, dftrain1.L_T1.values)
+plt.xcorr(dftrain1.L_T1.values, dftrain1.L_T3.values)
+
+corr = dftrain1.drop(columns = ['DATETIME', 'ATT_FLAG']).corr(method = 'spearman')
+
+# Generate a mask for the upper triangle
+mask = np.zeros_like(corr, dtype = np.bool)
+mask[np.triu_indices_from(mask)] = True
+
+# Set up the matplotlib figure
+f, ax = plt.subplots(figsize = (11, 9))
+
+# Generate a custom diverging colormap
+cmap = sns.diverging_palette(220, 10, as_cmap=True)
+
+# Draw the heatmap with the mask and correct aspect ratio
+sns.heatmap(corr, mask = mask, cmap = cmap, vmax = .3, center = 0,
+            square = True, linewidths=.5, cbar_kws={"shrink": .5})
+
+
+# %%
+
+fig = plt.figure()
+fx = fig.add_subplot(311, ylabel = 'water level (m)', 
+                     title = 'L_T1 vs L_T3')
+fx.plot(dftrain1.loc[100:172, 'L_T1'])
+sx = fig.add_subplot(312, sharex = fx, xlabel = 'Hour', 
+                     ylabel = 'water level (m)', ymargin=.5, yticks = [0, 1])
+sx.plot(dftrain1.loc[100:172, 'L_T3'])
+plt.setp(fx.get_xticklabels(), visible = False) #hide fx's xlabel
+fig.align_ylabels() #align y labels
+
+
+# %%
+
+## plot for valve 2: setting vs flowrate
+
+fig = plt.figure()
+fx = fig.add_subplot(211, ylabel = 'Flowrate (L/s)', 
+                     title = 'Valve 2: correlation between setting (S_V2) and flowrate (F_V2)')
+fx.plot(dftrain1.loc[0:72, 'F_V2'])
+sx = fig.add_subplot(614, sharex = fx, xlabel = 'Hour', 
+                     ylabel = 'Setting (dmnl)', ymargin=.5, yticks = [0, 1])
+sx.plot(dftrain1.loc[0:72, 'S_V2'])
+plt.setp(fx.get_xticklabels(), visible = False) #hide fx's xlabel
+fig.align_ylabels() #align y labels
+fig.savefig('.\image\comparison\compare-v2.png',
+            bbox_inches = 'tight', facecolor = 'w')
+
+# %%
+
+## plot for pump 7: setting vs flowrate
+
+fig = plt.figure()
+fx = fig.add_subplot(211, ylabel = 'Flowrate (L/s)', 
+                     title = 'Pump 7: correlation between setting (S_PU7) and flowrate (F_PU7)')
+fx.plot(dftrain1.loc[0:72, 'F_PU7'])
+sx = fig.add_subplot(614, sharex = fx, xlabel = 'Hour', 
+                     ylabel = 'Setting (dmnl)', ymargin=.5, yticks = [0, 1])
+sx.plot(dftrain1.loc[0:72, 'S_PU7'])
+plt.setp(fx.get_xticklabels(), visible = False) #hide fx's xlabel
+fig.align_ylabels() #align y labels
+fig.savefig('.\image\comparison\compare-pu7.png',
+            bbox_inches = 'tight', facecolor = 'w')
+
+# %%
+
+## plot for tank 3: vs pump 4 (setting & flowrate)
+
+fig = plt.figure()
+tx = fig.add_subplot(311, ylabel = 'Water level \n(meter)', 
+                     title = 'Tank 3: correlation with Pump 4 (S_PU4 & F_PU4)')
+tx.plot(dftrain1.loc[0:72, 'L_T3'])
+fx = fig.add_subplot(312, ylabel = 'Flowrate \n(L/s)')
+fx.plot(dftrain1.loc[0:72, 'F_PU4'])
+sx = fig.add_subplot(615, sharex = fx, xlabel = 'Hour', 
+                     ylabel = 'Setting \n(dmnl)', ymargin=.5, yticks = [0, 1])
+sx.plot(dftrain1.loc[0:72, 'S_PU4'])
+plt.setp(tx.get_xticklabels(), visible = False) #hide tx's xlabel
+plt.setp(fx.get_xticklabels(), visible = False) #hide fx's xlabel
+fig.align_ylabels() #align y labels
+fig.savefig('.\image\comparison\compare-lt3.png',
+            bbox_inches = 'tight', facecolor = 'w')
+# %%
+
+## plot for tank 4: vs pump 7 (setting & flowrate)
+
+fig = plt.figure()
+tx = fig.add_subplot(311, ylabel = 'Water level \n(meter)', 
+                     title = 'Tank 4: correlation with Pump 7')
+tx.plot(dftrain1.loc[0:72, 'L_T4'])
+fx = fig.add_subplot(312, ylabel = 'Flowrate \n(L/s)')
+fx.plot(dftrain1.loc[0:72, 'F_PU7'])
+sx = fig.add_subplot(615, sharex = fx, xlabel = 'Hour', 
+                     ylabel = 'Setting \n(dmnl)', ymargin=.5, yticks = [0, 1])
+sx.plot(dftrain1.loc[0:72, 'S_PU7'])
+plt.setp(tx.get_xticklabels(), visible = False) #hide tx's xlabel
+plt.setp(fx.get_xticklabels(), visible = False) #hide fx's xlabel
+fig.align_ylabels() #align y labels
+fig.savefig('.\image\comparison\compare-lt4.png',
+            bbox_inches = 'tight', facecolor = 'w')
+
+
+# %% [markdown]
+#
+# ### Visualisation of autocorrelations
+# 
+# - subset the dataset for one day
+# - plot per category of variables:
+#   - L_T#:    *Water level of tank # (meter)*
+#   - S/F_PU#: *Setting (dmnl) / flowrate (L/s) of pump #*
+#   - S/F_V# : *Setting (dmnl) / flowrate (L/s) of valve #*
+#   - P_J#:    *Pressure reading of node/junction # (meter)*
+# 
+
+# %%
+
+# plot autocoreelation of time series
+autocorrelation_plot(dftrain1.loc[0:72, 'L_T4'])
+plt.title('Autocorrelation of L_T4 (in a period of 3 days)')
+plt.savefig('.\image\comparison\Autocorrelation-lt4.png',
+            bbox_inches = 'tight', facecolor = 'w') #lag of 5 seems reasonable
+
+# %%
+
+# ARIMA
+model = ARIMA(dftrain1.loc[0:72, 'L_T4'], order = (5, 1, 0))
+
+model_fit = model.fit(disp = 0) #turn off debug information
+print(model_fit.summary())
+
+# residual errors
+residuals = pd.DataFrame(model_fit.resid)
+residuals.plot() #ARMA fit residual error line
+
+# %% [markdown]
+
+# ## Rolling Forecast ARIMA Model
+
+'''
+ARIMA, order = (p, d, q)
+p: AR, lag value for autoregression
+d: difference,  order of 1 to make the time series stationary
+q: MA
+'''
+
+# %%
+
+X = dftrain1['L_T4'].values
+size = int(len(X) * 0.66) # take 2/3 of the dataset for training
+train, test = X[0:size], X[size:len(X)]
+
+history = [x for x in train]
+predictions = list()
+
+for t in range(len(test)):
+    model = ARIMA(history, order = (5, 1, 0))
+    model_fit = model.fit(disp = 0) #discp=0: turn off debug information
+    output = model_fit.forecast()
+    yhat = output[0]
+    predictions.append(yhat)
+    obs = test[t]
+    history.append(obs)
+    #print('predicted = %0.2f, expected = %0.2f' % (yhat, obs))
+
+error = mean_squared_error(test, predictions)
+print('test MSE: %0.3f' % error)
+
+# plot the results
+plt.plot(test[0:72])
+plt.plot(predictions[0:72], color = 'red')
+plt.title('Water level of tank 4: real vs ARIMA rolling forecasted')
+plt.legend(('real', 'ARIMA, order = (5, 1, 0)'))
+plt.savefig('.\image\comparison\ARIMA-lt4.png',
+            bbox_inches = 'tight', facecolor = 'w')
 
 # %% [markdown]
 
@@ -134,6 +315,27 @@ dftrain2.columns = dftrain2.columns.str.strip()
 
 # Modify string date to timestamp
 dftrain2.DATETIME = dftrain2.DATETIME.apply(lambda s: string_to_timestamp(s))
+
+# %% [markdown]
+# 
+# ### Visualisation of correlation between variables
+# 
+# %%
+
+fig = plt.figure()
+tx = fig.add_subplot(311, ylabel = 'Water level \n(meter)', 
+                     title = 'Tank 4: correlation with Pump 7 (training dataset 2)')
+tx.plot(dftrain2.loc[0:72, 'L_T4'])
+fx = fig.add_subplot(312, ylabel = 'Flowrate \n(L/s)')
+fx.plot(dftrain2.loc[0:72, 'F_PU7'])
+sx = fig.add_subplot(615, sharex = fx, xlabel = 'Hour', 
+                     ylabel = 'Setting \n(dmnl)', ymargin=.5, yticks = [0, 1])
+sx.plot(dftrain2.loc[0:72, 'S_PU7'])
+plt.setp(tx.get_xticklabels(), visible = False) #hide tx's xlabel
+plt.setp(fx.get_xticklabels(), visible = False) #hide fx's xlabel
+fig.align_ylabels() #align y labels
+fig.savefig('.\image\comparison\compare-lt4-df2.png',
+            bbox_inches = 'tight', facecolor = 'w')
 
 # %% [markdown]
 #
@@ -220,6 +422,8 @@ dftest = pd.read_csv('./data/BATADAL_test_dataset.csv')
 # Check dataset
 dftest.sample(n = 15)
 dftest.info()
+
+dftest.S_PU1.unique()
 
 # %% [markdown]
 
